735e2bcafeca6767a68d922fc69d07053f276a7b
KathrinWirschem <51407394+KathrinWirschem@users.noreply.github.com>
2019-06-05 14:27:13 +0200

    Add files via upload

---
+++ power_residue_symbol.jl
@@ -0,0 +1,47 @@
+#d-th power residue symbol
+#(a/b)_d, b irreducible, d|q-1, F_q
+
+#gcd(a,b) != 1 => (a/b)_d = 0
+
+include("hoch.jl")
+
+function power_residue(a,b,d,symbol,q)
+  if b==0
+    return error("b!=0")
+  end
+
+  if gcd(a,b)!=1
+    a_b_d = 0
+    return a_b_d
+  end
+
+#kx = parent(a)
+#q = characteristic(base_ring(kx))
+
+  if degree(a)==0
+    return a^( div((q-1),d) * degree(b) )
+  end
+
+  a = mod(a,b)
+  lc = leading_coefficient(a)
+  F = factor(a)
+
+  symbol = symbol * hoch( lc, ( div((q-1),d) * degree(b) ) )
+  #push!(symbol, hoch( lc, ( div((q-1),d) * degree(b) ) ))
+
+  for i in F
+    symbol = symbol * hoch( (-1), div(q-1,d) * degree(b) * degree(i[1]) ) * (power_residue( b,i[1],d,1,q )) ^ (i[2])
+    #push!(symbol, hoch((-1), div(q-1,d) * degree(b) * degree(i[1])) * (power_residue( b,i[1],d,1,q )) ^ (i[2]))
+  end
+
+  return symbol
+
+
+#=
+##############################################################################
+
+  push!(symbol_factors, lc^( div((q-1),d) * degree(b) )
+
+##############################################################################
+=#
+end
