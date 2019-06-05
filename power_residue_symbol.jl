596975351d01be7570220213a97d718d838a72e6
KathrinWirschem <51407394+KathrinWirschem@users.noreply.github.com>
2019-06-05 22:02:43 +0200

    Add files via upload

--- power_residue_symbol.jl
+++ power_residue_symbol.jl
@@ -15,33 +15,21 @@ function power_residue(a,b,d,symbol,q)
     return a_b_d
   end
 
-#kx = parent(a)
-#q = characteristic(base_ring(kx))
-
-  if degree(a)==0
+  if degree(a)==0                               #a konstant -> Formel
     return a^( div((q-1),d) * degree(b) )
   end
 
-  a = mod(a,b)
-  lc = leading_coefficient(a)
+  a = mod(a,b)                                  #oberen Teil reduzieren
+  lc = leading_coefficient(a)                   #und dann in Faktoren aufteilen
   F = factor(a)
 
-  symbol = symbol * hoch( lc, ( div((q-1),d) * degree(b) ) )
-  #push!(symbol, hoch( lc, ( div((q-1),d) * degree(b) ) ))
+  symbol = symbol * hoch( lc, ( div((q-1),d) * degree(b) ) )    #lc konstant -> Formel
 
+  #einzelne Faktoren oben und unten tauschen -> rekursiver Aufruf
   for i in F
     symbol = symbol * hoch( (-1), div(q-1,d) * degree(b) * degree(i[1]) ) * (power_residue( b,i[1],d,1,q )) ^ (i[2])
-    #push!(symbol, hoch((-1), div(q-1,d) * degree(b) * degree(i[1])) * (power_residue( b,i[1],d,1,q )) ^ (i[2]))
   end
 
   return symbol
 
-
-#=
-##############################################################################
-
-  push!(symbol_factors, lc^( div((q-1),d) * degree(b) )
-
-##############################################################################
-=#
 end
