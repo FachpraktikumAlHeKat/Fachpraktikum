9176183538e4bbe1002aeb8746b5fef83bdf63b3
KathrinWirschem <51407394+KathrinWirschem@users.noreply.github.com>
2019-06-07 11:20:33 +0200

    Add files via upload

--- power_residue_symbol.jl
+++ power_residue_symbol.jl
@@ -1,13 +1,11 @@
-#d-th power residue symbol
-#(a/b)_d, b irreducible, d|q-1, F_q
-
-#gcd(a,b) != 1 => (a/b)_d = 0
+#d-th power residue symbol, Jacobi version
+#(a/b)_d, d|q-1, F_q
 
 include("hoch.jl")
 
 function power_residue(a,b,d,symbol,q)
-  if b==0
-    return error("b!=0")
+  if b==0 ||a==0
+    return error("a,b!=0")
   end
 
   if gcd(a,b)!=1
@@ -15,20 +13,16 @@ function power_residue(a,b,d,symbol,q)
     return a_b_d
   end
 
-  if degree(a)==0                               #a konstant -> Formel
+  if degree(a)==0                               #(!if b irreducible!) a konstant -> Formel
     return a^( div((q-1),d) * degree(b) )
   end
 
   a = mod(a,b)                                  #oberen Teil reduzieren
-  lc = leading_coefficient(a)                   #und dann in Faktoren aufteilen
-  F = factor(a)
 
-  symbol = symbol * hoch( lc, ( div((q-1),d) * degree(b) ) )    #lc konstant -> Formel
+  #oben und unten tauschen -> rekursiver Aufruf (allgemeines Reziprozitaetsgesetz)
+  symbol = hoch( -1, div(q-1, d) * degree(a) * degree(b) ) * hoch( leading_coefficient(a), degree(b) ) * hoch( leading_coefficient(b), (-degree(a)) ) * symbol(b,a,d,1,q)
 
-  #einzelne Faktoren oben und unten tauschen -> rekursiver Aufruf
-  for i in F
-    symbol = symbol * hoch( (-1), div(q-1,d) * degree(b) * degree(i[1]) ) * (power_residue( b,i[1],d,1,q )) ^ (i[2])
-  end
+
 
   return symbol
 
