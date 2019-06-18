19913a6bb17e920603b812fef73f09b23169842b
KathrinWirschem <51407394+KathrinWirschem@users.noreply.github.com>
2019-06-18 15:03:54 +0200

    Add files via upload
    ueberfluessige Dinge beseitigt (insbesondere Funktionsargumente)

--- power_residue_symbol.jl
+++ power_residue_symbol.jl
@@ -1,27 +1,29 @@
 #d-th power residue symbol, Jacobi version
 #(a/b)_d, d|q-1, F_q
+#e = q-1/d
 
-function power_residue(a,b,d,symbol,q)
-  q = Int(q)
+function power_residue(a,b,e)
 
-  if b==0 ||a==0
+  if b==0 || a==0
     return error("a,b!=0")
   end
 
   if gcd(a,b)!=1
-    a_b_d = 0
-    return a_b_d
+    return 0
   end
 
   a = mod(a,b)                                          #oberen Teil reduzieren
 
   if degree(a)==0                                       #oben konstant -> Formel
-    return a^( div((q-1),d) * Int(degree(b)) )
+    return a^( e * Int(degree(b)) )
   end
 
   #oben und unten tauschen -> rekursiver Aufruf (allgemeines Reziprozitaetsgesetz)
-  exp = div(q-1,d)
-  symbol = (-1)^Int( div(q-1, d) * degree(a) * degree(b) ) * leading_coefficient(a^exp)^Int(degree(b)) * leading_coefficient(b^exp)^Int(-degree(a)) * power_residue(b,a,d,1,q)
+  symbol = leading_coefficient(a)^( e*Int(degree(b)) ) * leading_coefficient(b)^( e*Int(-degree(a)) ) * power_residue(b,a,e)
+  if mod( e*degree(a)*degree(b), 2 )==1
+  #if last(base( fmpz(e*degree(a)*degree(b)), 2 ))=='1'
+      symbol = (-1) * symbol
+  end
 
   return symbol
 
