38ac38dd6736f81355b2ce93b1cee51ca830c7cc
KathrinWirschem <51407394+KathrinWirschem@users.noreply.github.com>
2019-06-18 14:18:11 +0200

    Add files via upload
    Klammern um -1...

--- power_residue_symbol.jl
+++ power_residue_symbol.jl
@@ -1,9 +1,9 @@
 #d-th power residue symbol, Jacobi version
 #(a/b)_d, d|q-1, F_q
 
-include("hoch.jl")
-
 function power_residue(a,b,d,symbol,q)
+  q = Int(q)
+
   if b==0 ||a==0
     return error("a,b!=0")
   end
@@ -13,19 +13,15 @@ function power_residue(a,b,d,symbol,q)
     return a_b_d
   end
 
-  a = mod(a,b)
+  a = mod(a,b)                                          #oberen Teil reduzieren
 
-#falsch!? ---------------------------------
-  if degree(a)==0                               #(!if b irreducible!) a konstant -> Formel
-    return a^( div((q-1),d) * degree(b) )
+  if degree(a)==0                                       #oben konstant -> Formel
+    return a^( div((q-1),d) * Int(degree(b)) )
   end
-#------------------------------------------
-
-  #a = mod(a,b)                                  #oberen Teil reduzieren
 
   #oben und unten tauschen -> rekursiver Aufruf (allgemeines Reziprozitaetsgesetz)
   exp = div(q-1,d)
-  symbol =  -1^Int(div(q-1, d) * degree(a) * degree(b) ) * leading_coefficient(a^exp)^Int(degree(b)) * leading_coefficient(b^exp)^Int(-degree(a)) * power_residue(b,a,d,1,q)
+  symbol = (-1)^Int( div(q-1, d) * degree(a) * degree(b) ) * leading_coefficient(a^exp)^Int(degree(b)) * leading_coefficient(b^exp)^Int(-degree(a)) * power_residue(b,a,d,1,q)
 
   return symbol
 
