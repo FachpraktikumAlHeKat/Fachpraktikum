9f4af7dc056b0e15a7232dd6c49f053a1ec67276
FachpraktikumAlHeKat <51407394+FachpraktikumAlHeKat@users.noreply.github.com>
2019-07-08 15:08:15 +0200

    Add files via upload

--- Baillie.jl
+++ Baillie.jl
@@ -9,26 +9,28 @@ kx,x = k["x"]
 function recursionstest(f,q)
 
 # mache dir eine Rekursionsfolge
-a=Array{nmod_poly}(undef,q^degree(f)+1)
+a=Array{nmod_poly}(undef,q^degree(f)+3)
+
 a[1]= x
-a[2]=x+1
-a[3]=a[1]+a[2]
-for i in 3:q^(degree(f))
+a[2]=x^2+8*x+18
+a[3]=mod(a[1]+a[2],f)				# schonmal festlegen, sonst meckert er!
+for i in 3:(q^(degree(f)))
 if a[i]!= a[2] || a[i-1]!= a[1] 
 	a[i+1]= mod(a[i]+a[i-1],f)
-	#i=i+1
 print(i ,"\n")
 else  # sonst a1=ai-1 und a2=ai
 
-	#d=divrem(q^degree(f)-1,i-2)
+	#d=divrem(q^(degree(f))-1,i-2)		# i-2 ist Periode, wenns ab i-1 wiederholt
 	#if d[2]==0
-	if mod(q^degree(f)-1,i-2)==0	
+	if mod(q^(degree(f))-1,i-2)==0	
 		return i, a[i], a[1], true
-	else return i, a[i], a[1], false
-		
 	end
+		
 end
+i=k
 end
+return a[k], a[1], false
+# wir haben immer bei i= 16 die rückgabe true- ich verstehe nur nicht warum
 end
 
 	
